4453014e69a1d39979f9b3f3dc44a47afb154ef0
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-05-20 17:11:28 +0530

    Setup Active Class

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,19 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/img/logo.png";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <Link className="navbar-brand" href="#">
+          <Link className="navbar-brand" href="/">
             <Image src={logo} alt="logo" className="logo" />
-            <h1>SG EVENTS</h1>
+            &nbsp;SgEvents
           </Link>
           <button
             className="navbar-toggler"
@@ -24,19 +28,40 @@ const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <Link className="nav-link" href="#">
+              <Link
+                className={`nav-link ${pathname === "/" ? "active" : ""}`}
+                href="/"
+              >
                 Home
               </Link>
-              <Link className="nav-link" href="/Features">
+              <Link
+                className={`nav-link ${
+                  pathname === "/Features" ? "active" : ""
+                }`}
+                href="/Features"
+              >
                 Features
               </Link>
-              <Link className="nav-link" href="/">
+              <Link
+                className={`nav-link ${
+                  pathname === "/Pricing" ? "active" : ""
+                }`}
+                href="/Pricing"
+              >
                 Pricing
               </Link>
-              <Link className="nav-link" href="/">
+              <Link
+                className={`nav-link ${
+                  pathname === "/Contact" ? "active" : ""
+                }`}
+                href="/Contact"
+              >
                 Contact
               </Link>
-              <Link className="nav-link" href="/">
+              <Link
+                className={`nav-link ${pathname === "/SignUp" ? "active" : ""}`}
+                href="/SignUp"
+              >
                 Sign Up
               </Link>
             </div>
