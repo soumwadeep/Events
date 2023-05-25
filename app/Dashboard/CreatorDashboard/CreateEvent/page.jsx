367ff1fc01521451aa22e140d70c5ffcaa994fc5
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-05-25 07:19:47 +0530

    Added Users Dashboard

---
+++ app/Dashboard/CreatorDashboard/CreateEvent/page.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/DashboardComponents/Sidebar"
+
+const page = () => {
+  return (
+    <div>
+        <Sidebar/>
+      <h1>Start Creating Events From Here</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ app/Dashboard/CreatorDashboard/ManageRsvp/page.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/DashboardComponents/Sidebar"
+
+const page = () => {
+  return (
+    <div>
+        <Sidebar/>
+      <h1>RSVP</h1>
+    </div>
+  )
+}
+
+export default page
