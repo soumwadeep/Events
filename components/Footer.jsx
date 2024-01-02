1be727007a81af292f3de02984206d28f1cc1923
Personal <soumwadeep@gmail.com>
2024-01-02 18:29:48 +0530

    Removed Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="footer" style={{display:"none"}}>
       <h5 className="text-center">
         Copyright @ Sg Events.All Rights Reserved.
       </h5>
